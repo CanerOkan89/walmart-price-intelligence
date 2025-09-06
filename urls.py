7af1effa3b0c8a23427df7c155904e3e237f8eb3
CanerOkan89 <canerokan1956@gmail.com>
2025-09-06 04:45:41 -0700

    urls.py

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tracker.urls')),
+    path('products/', views.ProductListView.as_view(), name='product-list'),
+    path('prices/', views.PriceEntryListView.as_view(), name='price-list'),
 ]
